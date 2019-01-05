5d38562dd4feafafc273badbe76674d041ef6bdf
root <root@beaglebone.localdomain>
2019-01-05 18:30:32 +0000

    add wait and delete ":"

--- get_netgear6100_macs.sh
+++ get_netgear6100_macs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ $# != 2 ]; then echo "[command] [user] [password]";fi
-mac_info=$(curl 10.0.0.1/DEV_show_device.htm -u $1:$2 -s | grep access_control_device.[0-9]*=)
+if [ $# != 2 ]; then 
+    echo "[command] [user] [password]"
+    exit 1;
+fi
+while [ "$mac_info" = "" ]
+do
+    sleep 1
+    mac_info=$(curl 10.0.0.1/DEV_show_device.htm -u $1:$2 -s | grep access_control_device.[0-9]*=)
+done
 mac_list=$(echo $mac_info | grep -E -o "[0-9A-Z]{2}(:[0-9A-Z]{2}){5}")
 for mac in $mac_list
 do
-  echo $mac
+  echo $mac | sed 's/://g'
 done
